64b2c3dc305744876fa88f9b0dc315d8a9b76776
olirvs <oliviamazone@gmail.com>
2025-12-08 17:07:46 +0100

    add class Diplome.java constructor and all getter

--- src/main/java/Diplome.java
+++ src/main/java/Diplome.java
@@ -1,21 +1,38 @@
+import java.util.HashMap;
+
 public class Diplome {
-    String name;
+    String nomDiplome;
     TypeDiplome type;
     int annee;
     int maxEtu;
     int ects;
 
-    public void Diplome(){
-        this.name = name;
+    public Diplome(String nomDiplome, TypeDiplome type, int annee, int maxEtu, int ects){
+        this.nomDiplome = nomDiplome;
         this.type = type;
         this.annee = annee;
         this.maxEtu = maxEtu;
         this.ects = ects;
     }
 
-    public boolean creerDiplome(){
-        //int tab[];
-        //completer
-        return false;
+    public String getName(){
+        return this.nomDiplome;
     }
+
+    public TypeDiplome getType(){
+        return this.type;
+    }
+
+    public int getAnnee(){
+        return this.annee;
+    }
+
+    public int getMaxEtu() {
+        return this.maxEtu;
+    }
+
+    public int getEcts(){
+        return this.ects;
+    }
+
 }
